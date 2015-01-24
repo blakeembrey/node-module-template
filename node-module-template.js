ef5a04482ec8c0f123cec2f412f595fdf9f98d86
Blake Embrey <hello@blakeembrey.com>
2015-01-24 10:15:57 +0900

    Add code templates

---
+++ node-module-template.js
@@ -0,0 +1,10 @@
+module.exports = template;
+
+/**
+ * Does something cool! :D
+ *
+ * @return {String}
+ */
+function template () {
+  return 'cool';
+}
--- test.js
+++ test.js
@@ -1,20 +1,9 @@
 /* global describe, it */
-var assert    = require('assert');
-var upperCase = require('./');
+var expect   = require('chai').expect;
+var template = require('./');
 
 describe('upper case', function () {
-  it('should upper case a string', function () {
-    assert.equal(upperCase(null), '');
-    assert.equal(upperCase('test'), 'TEST');
-    assert.equal(upperCase('TEST'), 'TEST');
-    assert.equal(upperCase('string'), 'STRING');
-  });
-
-  it('should support unicode', function () {
-    assert.equal(upperCase('\u0131'), 'I');
-  });
-
-  it('should support locale override', function () {
-    assert.equal(upperCase('i', 'tr'), '\u0130');
+  it('should do something cool', function () {
+    expect(template()).to.equal('cool');
   });
 });
