b08d9250cf5dc072f9c5c1e20b4f0a48a45d257a
Blake Embrey <hello@blakeembrey.com>
2015-01-24 11:24:05 +0900

    Correct test name

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var expect   = require('chai').expect;
 var template = require('./');
 
-describe('upper case', function () {
+describe('node module template', function () {
   it('should do something cool', function () {
     expect(template()).to.equal('cool');
   });
