9891e3214221bcb55361798c4e022f014ec9cbae
Blake Embrey <hello@blakeembrey.com>
2015-03-14 17:50:44 -0300

    update chai, add global comment and remove deps field

--- src/test.js
+++ src/test.js
@@ -1,8 +1,10 @@
+/* global describe, it */
+
 var expect = require('chai').expect
 var {{{moduleVariable}}} = require('./')
 
 describe('{{{moduleSentence}}}', function () {
-  it('should pass this test', function () {
+  it('should expose a function', function () {
     expect({{{moduleVariable}}}).to.be.a('function')
   })
 })
